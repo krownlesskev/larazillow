dcda0c4afad7e7151a802826952804ecd4a037c2
Kevin <kevin.sana@outlook.com>
2023-03-05 11:17:59 -0600

    Added new layout and props for home page

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <Link href="/">Home</Link>&nbsp;
+    <Link href="/hello">Show Page</Link>
+    <slot></slot>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+// defineProps({
+//     message: String,
+// });
+</script>
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
+    <MainLayout>
+        <div>Home</div>
+        <div>
+            {{ message }}
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import MainLayout from "../../Layouts/MainLayout.vue";
+defineProps({
+    message: String,
+});
 </script>
