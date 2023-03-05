f20c4858041aa7a2975f357b3ba4bc6f7cf8b231
Kevin <kevin.sana@outlook.com>
2023-03-05 16:37:50 -0600

    added reactive data

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,12 +1,16 @@
 <template>
     <Link href="/">Home</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
+    <div>Timer for this page: {{ timer }}</div>
     <slot></slot>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
-// defineProps({
-//     message: String,
-// });
+import { ref } from "vue";
+
+const timer = ref(0);
+setInterval(() => {
+    timer.value++;
+}, 1000);
 </script>
